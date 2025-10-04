2264b4987340f240466ef0c28fa4e26547afa907
dshmelov <dshmelov@yandex.ru>
2025-10-04 16:45:34 +0300

    [Лабораторная работа №2]
    Выполнил задание

--- lw2/lw2.py
+++ lw2/lw2.py
@@ -3,6 +3,26 @@ import os
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def equalize(grayscale_image: np.ndarray) -> np.ndarray:
+    hist = np.zeros(256, dtype=np.int_)
+    for v in grayscale_image.flatten():
+        hist[v] += 1
+
+    cdf = hist.cumsum()
+
+    min_cdf = cdf[np.nonzero(cdf)][0]
+
+    num_pixels = grayscale_image.size
+
+    cdf_normalized = ((cdf - min_cdf) * 255 / (num_pixels - min_cdf)).astype(dtype=np.uint8)
+
+    equalized_image = cdf_normalized[grayscale_image]
+
+    return equalized_image
+
 
 def main() -> None:
     """
@@ -10,20 +30,38 @@ def main() -> None:
     реализуйте данный вид эквализации самостоятельно
     """
     images_dir = 'data'
-
     images_names = os.listdir(images_dir)
 
     for image_name in images_names:
         image_path = os.path.join(images_dir, image_name)
 
-        bgr_image = cv.imread(filename=image_path)
-        rgb_image = cv.cvtColor(src=bgr_image, code=cv.COLOR_BGR2RGB)
+        grayscale_image = cv.imread(filename=image_path, flags=cv.IMREAD_GRAYSCALE)
+        equalized_image = equalize(grayscale_image=grayscale_image)
 
+        plt.figure(figsize=(12, 6))
 
+        plt.subplot(2, 2, 1)
+        plt.title('Исходное изображение')
+        plt.imshow(grayscale_image, cmap='gray')
+
+        plt.subplot(2, 2, 2)
+        plt.title('Гистограмма (до)')
+        plt.hist(grayscale_image.flatten(), bins=256, range=(0, 256), color='gray')
+
+        plt.subplot(2, 2, 3)
+        plt.title('Эквализированное')
+        plt.imshow(equalized_image, cmap='gray')
+
+        plt.subplot(2, 2, 4)
+        plt.title('Гистограмма (после)')
+        plt.hist(equalized_image.flatten(), bins=256, range=(0, 256), color='gray')
+
+        plt.show()
 
         pass
 
     pass
+
 
 if __name__ == '__main__':
     main()
